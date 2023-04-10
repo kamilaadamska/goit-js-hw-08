83de860b6cede36a5ea9b0821d644e0b273d3145
Kamila Adamska <121kama121@gmail.com>
2023-04-11 00:10:21 +0200

    Finished points 1 & 2 in task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.feedback-form');
 
+const savedValues = localStorage.getItem('feedback-form-state');
+
+if (savedValues) {
+  const parsedValues = JSON.parse(savedValues);
+  formEl.elements.email.value = parsedValues.email;
+  formEl.elements.message.value = parsedValues.message;
+}
+
+formEl.addEventListener('input', () => {
+  const formValues = {
+    email: formEl.elements.email.value,
+    message: formEl.elements.message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
+});
