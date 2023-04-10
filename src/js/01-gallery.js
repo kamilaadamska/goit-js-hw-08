0f7e88cba2981b545ab3c29d20205cff5703e540
Kamila Adamska <121kama121@gmail.com>
2023-04-10 15:07:19 +0200

    Finished SimpleLightBox task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+const galleryMarkup = [];
+
+function makeGallery(galleryObjects) {
+  for (const galleryObject of galleryObjects) {
+    const galleryItemMarkup = `<li>
+    <a class="gallery__link" href="${galleryObject.original}">
+    <img class="gallery__image" src="${galleryObject.preview}" alt="${galleryObject.description}" />
+    </a>
+    </li>`;
+
+    galleryMarkup.push(galleryItemMarkup);
+  }
+  galleryListEl.innerHTML = galleryMarkup.join('');
+}
+
+makeGallery(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: !0,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
