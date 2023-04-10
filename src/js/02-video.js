063152517c45e96ff456dada15be8367713c7925
Kamila Adamska <121kama121@gmail.com>
2023-04-10 16:37:33 +0200

    Updated saving current time of videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', e => {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+});
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime).then();
